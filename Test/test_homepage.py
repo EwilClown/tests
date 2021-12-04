9d46e08fdca785a7fc558e35966bed818fb8ffab
Clown <clown_email@mail.ru>
2021-12-05 01:14:42 +0300

    suc

---
+++ Test/test_homepage.py
@@ -0,0 +1,12 @@
+import pytest
+
+from pom.homepage_nav import HomepageNav
+
+
+@pytest.mark.usefixtures('setup')
+class TestHomepage:
+
+    def test_nav_links(self):
+        homepage_nav = HomepageNav(self.driver)
+        for indx in range(8):
+            homepage_nav.get_nav_links()[indx].click()
